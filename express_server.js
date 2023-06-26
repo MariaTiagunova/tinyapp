ae4813b38dbfa4ccfca956e4da64595a022a1a09
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-26 13:50:13 -0400

    Add POST route for /register, set user_id cookie

--- express_server.js
+++ express_server.js
@@ -7,11 +7,22 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Contains ids(short URLs) and corresponding long URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Contains user data
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+};
+
+// Generates random string with 6 symbols for short URL id and User id
 function generateRandomString() {
   let randomString = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -34,23 +45,38 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/register", (req, res) => {
+  const userID = generateRandomString();
+  users[userID] = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  res.cookie('user_id', userID)
+  console.log(users);
+  res.redirect("/urls");
+  });
+
 app.get("/urls", (req, res) => {
+  const userID = req.cookies["user_id"]
   const templateVars = { 
-    username: req.cookies["username"],
+    user: users[userID],
     urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const userID = req.cookies["user_id"]
   const templateVars = { 
-    username: req.cookies["username"]
+    user: users[userID]
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+  const userID = req.cookies["user_id"]
   const templateVars = { 
-    username: req.cookies["username"],
+    user: users[userID],
     id: req.params.id,
     longURL: urlDatabase.id };
   res.render("urls_show", templateVars);
@@ -81,19 +107,21 @@ app.post("/urls/:id", (req, res) => { // edit long url
 
 app.post("/login", (req, res) => { 
 // set a cookie named username to the value submitted in the request body
-const username = req.body.username;
-res.cookie('username', username);
+const userID = req.cookies["user_id"]
+  const templateVars = { 
+    user: users[userID]
+  }
 res.redirect('/urls');
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
 app.get("/register", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   }
   res.render("register", templateVars);
 });
